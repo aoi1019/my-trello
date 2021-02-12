403ae3227331cdace83795c8a7021aa80ad509b2
前田蒼生 <aoi1019m@gmail.com>
2021-02-12 21:25:36 +0900

    add: コミット

---
+++ src/components/Board.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <header>
+      My Trello
+    </header>
+    <main>
+      <p class="info-line">All: {{ totalCardCount }} tasks</p>
+      <draggable :list="lists" 
+        class="list-index"
+        @end="movingList">
+        <list v-for="(item, index) in lists"
+          :key="item.id" 
+          :title="item.title" 
+          :cards="item.cards" 
+          :listIndex="index" 
+          @change="movingCard" 
+         />
+      <list-add />
+      </draggable>
+    </main>
+  </div>
+</template>
+
+<script>
+import ListAdd from './ListAdd.vue'
+import List from './List'
+import { mapState } from 'vuex'
+import draggable from 'vuedraggable'
+export default {
+  components: {
+    ListAdd,
+    List,
+    draggable
+  },
+  computed: {
+    ...mapState([
+      'lists'
+    ]),
+    totalCardCount: function(){
+      return this.$store.getters.totalCardCount
+    }
+  },
+  methods: {
+    movingCard: function() {
+      this.$store.dispatch('updateList', { lists: this.lists })
+    },
+    movingList: function() {
+      this.$store.dispatch('updateList', { lists: this.lists })
+    }
+  }
+}
+</script>
---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card">
+    <button class="close-button" @click="removeCardFromList">
+      ×
+    </button>
+    <div class="body">
+      {{ body }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    body: {
+      type: String,
+      required: true
+    },
+    listIndex: {
+      type: Number,
+      required: true
+    },
+    cardIndex: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    removeCardFromList() {
+      this.$store.dispatch('removeCardFromList', {cardIndex: this.cardIndex, listIndex: this.listIndex})
+    }
+  }
+
+}
+</script>
